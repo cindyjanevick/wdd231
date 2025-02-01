28a322cd177a802da33fd147313d4b4d3a82e7d6
Cindy Jane Vick <cindyjanevick@gmail.com>
2025-02-02 04:00:05 +0900

    js

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const cards = document.querySelectorAll(".card");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const cards = document.querySelectorAll(".card");
 
-    cards.forEach(card => {
-        const modal = card.querySelector(".membership-dialog");
-        const closeButton = modal.querySelector(".close-button");
+//     cards.forEach(card => {
+//         const modal = card.querySelector(".membership-dialog");
+//         const closeButton = modal.querySelector(".close-button");
 
-        // Open the modal when the card is clicked
-        card.addEventListener("click", () => {
-            if (!modal.open) {
-                modal.showModal();
-            }
-        });
+//         // Open the modal when the card is clicked
+//         card.addEventListener("click", () => {
+//             if (!modal.open) {
+//                 modal.showModal();
+//             }
+//         });
 
-        // Close the modal when the close button is clicked
-        closeButton.addEventListener("click", (event) => {
-            modal.close();
-            event.stopPropagation(); // Prevent triggering card click event when closing
-        });
-    });
-});
+//         // Close the modal when the close button is clicked
+//         closeButton.addEventListener("click", (event) => {
+//             modal.close();
+//             event.stopPropagation(); // Prevent triggering card click event when closing
+//         });
+//     });
+// });
