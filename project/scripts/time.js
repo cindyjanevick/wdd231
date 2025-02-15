3319841b8cc4d2ea3cdfaae666ce69e10679c0e3
Cindy Jane Vick <cindyjanevick@gmail.com>
2025-02-16 01:26:59 +0900

    time

--- project/scripts/time.js
+++ project/scripts/time.js
@@ -1,7 +1,6 @@
-// Time script
 function updateTime() {
-    const currentTime = new Date().toLocaleString();
-    document.getElementById('current-time').innerHTML = `Current Time in Tokyo: ${currentTime}`;
+    const tokyoTime = new Date().toLocaleString('en-US', { timeZone: 'Asia/Tokyo' });
+    document.getElementById('current-time').innerHTML = `Current Time in Tokyo: ${tokyoTime}`;
 }
 
 setInterval(updateTime, 1000); // Update every second
